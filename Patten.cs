ce2a53b381093e1e1044c49b6573c2c928c3f8d0
urvis <urvis@LAPTOP-ETPFH4CU>
2024-12-20 09:01:55 +0530

    Add project files.

---
+++ Patten.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Patten
+{
+    static void Main()
+    {
+        int rows = 5;
+
+        for (int i = 1; i <= rows; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1_BD_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int r = 5;
+            Console.WriteLine("Area Of Circle Is: "+(3.14*r*r));
+            Console.Read();   
+        }
+    }
+}
